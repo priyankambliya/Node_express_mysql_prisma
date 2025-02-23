ef9db6284e0c46486f2e419f6ea546c13e068347
priyankambliya <priyankambliya18@gmail.com>
2025-02-24 00:06:54 +0530

    remove express-session dependency and related code from the application

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import "./providers/connection.provider"
 import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
-import session from 'express-session'
 
 import routes from './routes'
 import handler from "./utils/handler"
@@ -9,15 +8,6 @@ import rateLimiter from "./middlewares/rateLimiter"
 import corsOptions from "./utils/corsOptions"
 
 export const app: Express = express()
-
-app.use(session({
-    secret: process.env['SESSION_SECRET'] as string,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24, // 1 day
-    }
-}))
 
 app.use(cors(corsOptions))
 app.use(rateLimiter())
